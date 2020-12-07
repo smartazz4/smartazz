f8e98ea97bdf32f737c03b6160da00b2742ce075
demhademha <ahmedtexas05@gmail.com>
2020-12-07 19:57:40 +0000

    welcome them

---
+++ welcome.c
@@ -0,0 +1,7 @@
+#include  "colours.h"
+#include <stdio.h>
+int main ()
+{
+printf(COLOUR_GREEN"Welcome to SuccessionCLI, written by Samg_is_a_Ninja and Hassan’s Tech (demhademha) \n Special thanks to pwn20wnd (mountpoint and rsync args), The Procursus Team (for providing the procursus build system  used to build the command line tools) and shmoopi (for iOS System Services) \n If you found this tool useful, then consider donating to demhademha at https://www.paypal.me/demhademha and to Samg_is_a_Ninja at https://www.paypal.me/SamGardner4 In addition, you can visit https://github.com/Samgisaninja/SuccessionRestore/tree/SuccessionCLI to get support \n"COLOUR_RESET);
+return 0;
+}
